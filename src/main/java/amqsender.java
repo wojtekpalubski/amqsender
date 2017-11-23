c597236f61b318d920d2302fcb92d988cf26a66c
qq <w@p>
2017-11-23 22:35:19 +0100

    synchroniczny

--- src/main/java/amqsender.java
+++ src/main/java/amqsender.java
@@ -10,11 +10,20 @@ public class amqsender {
         System.out.println("ClientID: "+connection.getClientID());
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Queue queueRequest=session.createQueue("RequestQ");
+        Queue queueResponse=session.createQueue("ResponseQ");
         TextMessage msg= session.createTextMessage("wiadomosc 1");
+        msg.setJMSReplyTo(queueResponse);
         MessageProducer sender= session.createProducer(queueRequest);
         sender.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
         sender.send(msg);
         System.out.println("Wyslano: >"+msg.getText()+"< "+msg.getJMSMessageID()+" "+msg.getJMSCorrelationID());
+
+        String filtr = "JMSCorrelationID = '"+msg.getJMSMessageID()+"'";
+        System.out.println("Czekam na odpowiedz "+filtr);
+        MessageConsumer receiver= session.createConsumer(queueResponse, filtr);
+        TextMessage msgResponse=(TextMessage)receiver.receive();
+        System.out.println("Odebrano: >"+msgResponse.getText()+"< "+msgResponse.getJMSMessageID()+" "+msgResponse.getJMSCorrelationID());
+        receiver.close();
         sender.close();
         session.close();
         connection.stop();
